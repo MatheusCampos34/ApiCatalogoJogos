f0919e64c634a5e06431eba5640f1167ba9af88c
matheuscamposbr34 <matheuscamposbr34@gmail.com>
2021-09-19 19:48:28 -0300

    Teste!

---
+++ ExemploApiCatalogoJogos/Entities/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Entities
+{
+    public class Jogo
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+
+        public string Console { get; set; }
+        public double Ano { get; set; }
+    }
+}
---
+++ ExemploApiCatalogoJogos/Repositories/JogoRepository.cs
@@ -0,0 +1,75 @@
+﻿using ExemploApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploApiCatalogoJogos.Repositories
+{
+    public class JogoRepository : IJogoRepository
+    {
+        private static Dictionary<Guid, Jogo> jogos = new Dictionary<Guid, Jogo>()
+        {
+            {Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), new Jogo{ Id = Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), Nome = "Mario", Produtora = "Nintendo", Ano = 1985, Console = "SNES", Preco = 60} },
+            {Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), new Jogo{ Id = Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), Nome = "Donkey Kong", Produtora = "Nintendo", Ano = 1981, Console = "Arcade, Colecovision, Atari 2600, Intellivision, Game & Watch, NES, Game Boy, SNES",  Preco = 190} },
+            {Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), new Jogo{ Id = Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), Nome = "Sonic", Produtora = "Sega", Ano = 1991, Console = "Master System",  Preco = 100} },
+            {Guid.Parse("da033439-f352-4539-879f-515759312d53"), new Jogo{ Id = Guid.Parse("da033439-f352-4539-879f-515759312d53"), Nome = "Contra 3", Produtora = "Konami", Ano = 1992, Console = "SNES Game Boy Game Boy Advance Virtual Console",  Preco = 85} },
+            {Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), new Jogo{ Id = Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), Nome = "Sim City", Produtora = "Maxis", Ano = 1989, Console = "Sega Saturn, SNES",  Preco = 70} },
+            {Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), new Jogo{ Id = Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), Nome = "Double Dragon", Produtora = "Arc System Works", Ano = 1985, Console = "NES, Atari 7800",  Preco = 132} }
+        };
+
+        public Task<List<Jogo>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina - 1) * quantidade).Take(quantidade).ToList());
+        }
+
+        public Task<Jogo> Obter(Guid id)
+        {
+            if (!jogos.ContainsKey(id))
+                return Task.FromResult<Jogo>(null);
+
+            return Task.FromResult(jogos[id]);
+        }
+
+        public Task<List<Jogo>> Obter(string nome, string produtora)
+        {
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora)).ToList());
+        }
+
+        public Task<List<Jogo>> ObterSemLambda(string nome, string produtora)
+        {
+            var retorno = new List<Jogo>();
+
+            foreach(var jogo in jogos.Values)
+            {
+                if (jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora))
+                    retorno.Add(jogo);
+            }
+
+            return Task.FromResult(retorno);
+        }
+
+        public Task Inserir(Jogo jogo)
+        {
+            jogos.Add(jogo.Id, jogo);
+            return Task.CompletedTask;
+        }
+
+        public Task Atualizar(Jogo jogo)
+        {
+            jogos[jogo.Id] = jogo;
+            return Task.CompletedTask;
+        }
+
+        public Task Remover(Guid id)
+        {
+            jogos.Remove(id);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            //Fechar conexão com o banco
+        }
+    }
+}
